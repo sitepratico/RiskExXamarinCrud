c6ddc4ec7d1ef15d05f5994e974d3c002e0896fe
Wanderson Andrade <atendimento@sitepratico.com>
2021-07-01 03:41:04 -0300

    Add project files.

---
+++ RiskExXamarinCrud/RiskExXamarinCrud/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RiskExXamarinCrud.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RiskExXamarinCrud.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ RiskExXamarinCrud/RiskExXamarinCrud/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using RiskExXamarinCrud.Models;
+using RiskExXamarinCrud.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RiskExXamarinCrud.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
